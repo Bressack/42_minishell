657e44c190c7449e744eeffe6be4cc2ad6291f3e
tarchen <tarchen@student.42.fr>
2020-02-27 00:39:31 +0100

    auto commit from alias

--- git__get_current_branch_name.c
+++ git__get_current_branch_name.c
@@ -6,12 +6,13 @@
 /*   By: tharchen <tharchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/23 16:33:56 by tharchen          #+#    #+#             */
-/*   Updated: 2020/02/23 20:15:25 by tharchen         ###   ########.fr       */
+/*   Updated: 2020/02/27 00:39:23 by tharchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <libc.h>
-# define CURR_DIR	"/Users/tharchen/Prog/42/circle_4/minishell"
+// # define CURR_DIR	"/Users/tharchen/Prog/42/circle_4/minishell"
+# define CURR_DIR	"/Users/user/Prog/42/projects/circle_4/minishell"
 
 static void	*join(char **ret, char *arg, int *retsize)
 {
@@ -59,26 +60,41 @@ char		*ft_strjoin(int nb_str, ...)
 }
 
 
-char		*git__get_current_branch_name(char **av, char **env)
+char		*git__get_current_branch_name(char **env)
 {
 	pid_t	pid;
-	char	**avv;
+	char	**av;
 	int		stt;
 
-	avv = malloc(sizeof(char *) * 3);
-	avv[0] = strdup("/bin/cat");
-	avv[1] = ft_strjoin(3, CURR_DIR, "/", ".git/HEAD");
-	avv[2] = NULL;
+	int		fd[2];
+
+	if (pipe(fd) != 0)
+		return (NULL);
+	av = malloc(sizeof(char *) * 3);
+	av[0] = strdup("/bin/cat");
+	av[1] = ft_strjoin(3, CURR_DIR, "/", ".git/HEAD");
+	av[2] = NULL;
 	if (!(pid = fork()))
 	{
-		execve("/bin/cat", avv, env);
+		close(0);
+		fd[1] = dup(0);
+		dprintf(fd[1], "CACA\n");
+		execve("/bin/cat", av, env);
 		exit(-1);
 	}
 	waitpid(pid, &stt, WUNTRACED);
 	printf("$?: %d\n", stt);
-	free(avv[0]);
-	free(avv[1]);
-	free(avv);
+	free(av[0]);
+	free(av[1]);
+	free(av);
+
+	char	buf[1024 + 1];
+	int		ret;
+
+	ret = read(fd[0], buf, 1024);
+	buf[ret] = 0;
+
+	printf("read: [%s]\n", buf);
 	return (NULL);
 }
 
@@ -86,7 +102,7 @@ int		main(int ac, char **av, char **env)
 {
 	(void)ac;
 	(void)av;
-	git__get_current_branch_name(av, env);
+	git__get_current_branch_name(env);
 	return (0);
 }
 
