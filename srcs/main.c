a8341c4856123e538153f688150d1f7db86b3955
tarchen <tarchen@student.42.fr>
2020-03-01 14:40:35 +0100

    ugh

--- srcs/main.c
+++ srcs/main.c
@@ -6,11 +6,7 @@
 /*   By: tharchen <tharchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 12:52:12 by tharchen          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2020/03/01 13:38:30 by tharchen         ###   ########.fr       */
-=======
-/*   Updated: 2020/02/29 15:39:36 by fredrikalindh    ###   ########.fr       */
->>>>>>> fredwtoken
+/*   Updated: 2020/03/01 14:39:58 by tharchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,88 +49,30 @@ void	sig_handler(int signo)
 		ft_dprintf(0, "\b\b  \b\b");
 }
 
-<<<<<<< HEAD
-// void	sigq_handler(int signo)
-// {
-// 	if (signo == SIGINT)
-// 	{
-// 		write(1, "\n", 1);
-// 		print_prompt(PROMPT_CASUAL);
-// 		signal(SIGINT, sig_handler);
-// 	}
-// }
-
-int		get_deep(t_node *root, int i)
-{
-	int	lret;
-	int	rret;
-
-	if (root->left)
-		lret = get_deep(root, i + 1);
-	else
-		lret = i;
-	if (root->right)
-		rret = get_deep(root, i + 1);
-	else
-		rret = i;
-	return (lret > rret ? lret : rret);
-}
-
-=======
->>>>>>> fredwtoken
 int		main(int ac, char **av, char **env)
 {
 	t_lexer		lex;
 	t_token		*current_token;
 	int			sloc;
-<<<<<<< HEAD
 	t_node		*ast;
-=======
 	// t_ast		ast;
 	t_token		*args;
 	t_token		*trav;
 	t_token		*prev;
->>>>>>> fredwtoken
 
 	g_all_malloc = NULL;
 	bzero(&lex, sizeof(t_lexer));
 	get_env(ac, av, env);
-	// printf("echo $HOME \"je \'suis\' un \\\" chat\"$OK$SALUT\\$HOME\n");
-	sloc = 0;
 	while (1)
 	{
 		signal(SIGINT, sig_handler);
 		signal(SIGQUIT, sig_handler);
-		lexer__refill_line(&lex, sloc, PROMPT_CASUAL);
 		if (lex.line[0] < 0 && ft_dprintf(1, "exit\n") && !free_all_malloc()) // CALL XEXIT INSTEAD ?
 			break ;
 		// ast = ast__new(lex);
 		args = NULL;
 		while (1)
 		{
-			current_token = lexer__get_next_token(&lex);
-			// token__print(current_token);
-			if (current_token->type == ERR)
-			{
-				lexer__error(1, &lex);
-				break ;
-			}
-			if (token__istype(current_token, EOT | NONE) && !(trav->next = NULL))
-				break ;
-			trav = (t_token *)mmalloc(sizeof(t_token));
-			if (args == NULL)
-				args = trav;
-			else
-				prev->next = trav;
-			prev = trav;
-			trav->value = ft_strdup(current_token->value);
-			// token__del(current_token);
-			// ast__add_node(&ast, current_token);
-
-<<<<<<< HEAD
-		// int			pid;
-		// if (!(pid = fork()))
-		// {
 			ast = ast_builder(sloc);
 			if (ast)
 			{
@@ -148,11 +86,9 @@ int		main(int ac, char **av, char **env)
 		if (sloc == 6)
 			printf("[ SEGV  ] You got a segv lmao, you\'re so bad\n");
 		// return (sloc);
-=======
 		}
 		execute(args);
 		try_free_((void **)&lex.line, _FL_);
->>>>>>> fredwtoken
 	}
 	try_free_((void **)&lex.line, _FL_);
 	// print_mem__(PRINT_ERR);
--- srcs/utils/lists/ft_add_node_end_np.c
+++ srcs/utils/lists/ft_add_node_end_np.c
@@ -6,7 +6,7 @@
 /*   By: tharchen <tharchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/28 16:28:23 by tharchen          #+#    #+#             */
-/*   Updated: 2020/03/01 13:37:01 by tharchen         ###   ########.fr       */
+/*   Updated: 2020/03/01 14:40:16 by tharchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,41 @@ void		ft_add_node_end_np(t_pnp **begin_list, t_pnp *new)
 {
 	t_pnp	*tmp;
 
+	new = new->next;
+	// t_token	*new_t = (t_token *)new;
+	// t_token	*begin_list_t = (t_token *)*begin_list;
+	//
+	// printf(""C_G_YELLOW"new        : %p -> %p -> %p\n"C_RES,
+	// new_t ? new_t->prev : NULL,
+	// new_t,
+	// new_t ? new_t->next : NULL);
+	//
+	// printf(""C_G_YELLOW"new        : %s -> %s -> %s\n"C_RES,
+	// new_t && new_t->prev ? new_t->prev->value : NULL,
+	// new_t->value,
+	// new_t && new_t->next ? new_t->next->value : NULL);
+	//
+	// new_t = new_t->next;
+	// printf(""C_G_YELLOW"new        : %s -> %s -> %s\n"C_RES,
+	// new_t && new_t->prev ? new_t->prev->value : NULL,
+	// new_t->value,
+	// new_t && new_t->next ? new_t->next->value : NULL);
+	//
+	// printf("begin_list : %p -> %p -> %p\n",
+	// begin_list_t ? begin_list_t->prev : NULL,
+	// begin_list_t,
+	// begin_list_t ? begin_list_t->next : NULL);
+
 	if (!(*begin_list))
 	{
+		printf(TEST);
 		*begin_list = new;
 		new->next = NULL;
 		new->prev = NULL;
 	}
 	else
 	{
+		printf(TEST);
 		tmp = *begin_list;
 		while (tmp && tmp->next)
 			tmp = tmp->next;
@@ -31,8 +58,8 @@ void		ft_add_node_end_np(t_pnp **begin_list, t_pnp *new)
 		new->prev = tmp;
 		new->next = NULL;
 	}
-	// printf(""C_G_GRAY"\n*********************************************************************************\n"C_G_RED"LIST ARG:"C_RES" ");
-	// for (t_token *tmp2 = (t_token *)*begin_list ; tmp2 ; tmp2 = tmp2->next)
-	// 	printf("[%s]", tmp2->value);
-	// printf("\n");
+	printf(""C_G_RED"LIST ARG:"C_RES" ");
+	for (t_token *tmp2 = (t_token *)*begin_list ; tmp2 ; tmp2 = tmp2->next)
+		printf("[%s]", tmp2->value);
+	printf("\n");
 }
