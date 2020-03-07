314076563f28e0544ec8b1a089df7437c4128dbb
tharchen <tharchen@student.42.fr>
2020-03-07 18:44:20 +0100

    AST_INTERPRETER COMMENTED.

--- srcs/ast_interpreter/ast_interpreter.c
+++ srcs/ast_interpreter/ast_interpreter.c
@@ -6,11 +6,18 @@
 /*   By: tharchen <tharchen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/01 19:26:24 by tharchen          #+#    #+#             */
-/*   Updated: 2020/03/06 22:56:08 by tharchen         ###   ########.fr       */
+/*   Updated: 2020/03/07 18:43:51 by tharchen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+/*
+** THIS FUNCTION PRINT ERRORS ON STDERR
+**
+** ERR_OPEN : ERROR FROM open() (strerror() RETURN THE MESSAGE)
+** ERR_PIPE : ERROR FROM pipe() (strerror() RETURN THE MESSAGE)
+*/
 
 int		asti_error(char *name, int opt)
 {
@@ -21,11 +28,32 @@ int		asti_error(char *name, int opt)
 		return (ERROR);
 }
 
+/*
+** THIS FUNCTION RETURN 1 IF THE PARENT OF THE NODE IS A PIPE
+** ELSE IT RETURN 0
+*/
+
 int		node__parent_ispipe(t_node *node)
 {
 	return (node && node->parent && node->parent->type == SEP &&
 		node->parent->sep->type == PIPE);
 }
+
+
+/*
+** THIS FUNCTION OPEN A NEW PIPE AND ASSIGN THE WRITE SIDE TO THE LEFT CHILD
+** AND THE READ SIDE TO THE RIGHT ONE.
+**
+** IT CLOSE THE STDOUT FD OF THE LEFT CHILD IF DIFFERENT STDOUT (1)
+** IT CLOSE THE STDIN FD OF THE RIGHT CHILD IF DIFFERENT STDIN (0)
+**
+** RUN THE LEFT CHILD THEN CLOSE THE WRITE SIDE OF THE PIPE CAUSE THE LEFT CHILD
+** HAS FINISHED TO USE IT
+**
+** RUN THE RIGHT CHILD THEN CLOSE THE READ SIDE OF THE PIPE CAUSE THE RIGHT
+** CHILD HAS FINISHED TO USE IT
+*/
+
 
 int		node__pipe_handle(t_node *ppln)
 {
@@ -47,6 +75,13 @@ int		node__pipe_handle(t_node *ppln)
 	return (sloc);
 }
 
+/*
+** THIS FUNCTION RUN THE LEFT CHILD OF THE DBL_AND NODE THEN WAIT FOR ITS END
+** IF THE LEFT CHILD SUCCEEDED SO RUN THE RIGHT CHILD AND WAIT FOR ITS END
+** RETURN THE RETURN VALUE OF THE LEFT CHILD IF IT HAS FAILED ELSE IT RETURN
+** THE RETURN OF THE RIGHT CHILD ELSE
+*/
+
 int		node__dbl_and_handle(t_node *cmd_sep)
 {
 	int	sloc;
@@ -61,6 +96,13 @@ int		node__dbl_and_handle(t_node *cmd_sep)
 	}
 	return (sloc);
 }
+
+/*
+** THIS FUNCTION RUN THE LEFT CHILD OF THE DBL_OR NODE THEN WAIT FOR ITS END
+** IF THE LEFT CHILD FAILED SO RUN THE RIGHT CHILD AND WAIT FOR ITS END
+** RETURN THE RETURN VALUE OF THE LEFT CHILD IF IT HAS SUCCEEDED ELSE IT RETURN
+** THE RETURN OF THE RIGHT CHILD ELSE
+*/
 
 int		node__dbl_or_handle(t_node *cmd_sep)
 {
@@ -77,6 +119,12 @@ int		node__dbl_or_handle(t_node *cmd_sep)
 	return (sloc);
 }
 
+/*
+** THIS FUNCTION RUN THE LEFT CHILD OF THE SEMICON NODE THEN WAIT FOR ITS END
+** IF THE RIGHT CHILD EXIST (NOT NULL) THEN RUN IT AND WAIT FOR ITS END
+** RETURN THE LAST RETURN LEFT OR RIGHT
+*/
+
 int		node__semicon_handle(t_node *cmd_sep)
 {
 	int	sloc;
@@ -92,6 +140,15 @@ int		node__semicon_handle(t_node *cmd_sep)
 	return (sloc);
 }
 
+/*
+** THIS FUNCTION RUN THE GOOD FUNCTION TO HANDLE SEPARATORS.
+** node__pipe_handle FOR PIPES
+** node__dbl_and_handle FOR DBL_AND SEPARATOR
+** node__dbl_or_handle FOR DBL_OR SEPARATOR
+** node__semicon_handle FOR SEMICON SEPARATOR
+** RETURN ERROR IF THE NODE IS NOT A GOOD SEP
+*/
+
 int		node__sep_controller(t_node *sep)
 {
 	if (token__istype(sep->sep, PIPE))
@@ -104,8 +161,14 @@ int		node__sep_controller(t_node *sep)
 		return (node__semicon_handle(sep));
 	else
 		return (ERROR);
-	return (SUCCESS);
 }
+
+/*
+** THIS FUNCTION WILL OPEN ALL REDIRECTION AND ASSIGN TO cmd->stdout OR
+** cmd->stdin
+** THE FD JUST OPENED. BEFORE THE ASSIGNMENT, IT CLOSE THE FD PRESENT IN
+** cmd->stdout OR cmd->stdin IF THEY ARE DIFFERENT OF STDOUT OR STDIN
+*/
 
 int		redir_handle(t_node *cmd)
 {
@@ -144,6 +207,12 @@ int		redir_handle(t_node *cmd)
 	return (SUCCESS);
 }
 
+/*
+** THIS FUNCTION WILL HANDLE REDIRECTION OF THE CMD FIRST (THAT WILL OVERWRITES
+** PIPES WHO COULD BE OPENED BEFORE) AND THEN RUN THE GOOD FUNCTION TO RUN A
+** COMMAND
+*/
+
 int		node__cmd_controller(t_node *cmd)
 {
 	if (redir_handle(cmd) == ERROR)
@@ -153,6 +222,13 @@ int		node__cmd_controller(t_node *cmd)
 		waitpid(cmd->pid, (int *)&g_exit, WUNTRACED);
 	return (g_exit);
 }
+
+/*
+** THIS FUNCTION WILL RUN THE GOOD FUNCTION TO HANDLE THE CURRENT NODE
+** IF SEP -> RUN node__sep_controller
+** ELSE IF CMD -> RUN node__cmd_controller
+** ELSE JUST RETURN SUCCESS (EMPTY EXPRESSION)
+*/
 
 int		node__controller(t_node *node)
 {
